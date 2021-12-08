6d49714d0d9faf7e1bcb044de33c64c65cd7bf96
Alkaid <>
2021-12-08 19:31:09 +0800

    cmake -DCMAKE_BUILD_TYPE=Debug -DOPENSSL_ROOT_DIR=`brew --prefix openssl` -DOPENSSL_LIBRARIES=`brew --prefix openssl`

---
+++ src/util/CMakeFiles/util.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/alkaid/Projects/workflow/src/util/EncodeStream.cc" "src/util/CMakeFiles/util.dir/EncodeStream.cc.o" "gcc" "src/util/CMakeFiles/util.dir/EncodeStream.cc.o.d"
+  "/Users/alkaid/Projects/workflow/src/util/MD5Util.cc" "src/util/CMakeFiles/util.dir/MD5Util.cc.o" "gcc" "src/util/CMakeFiles/util.dir/MD5Util.cc.o.d"
+  "/Users/alkaid/Projects/workflow/src/util/StringUtil.cc" "src/util/CMakeFiles/util.dir/StringUtil.cc.o" "gcc" "src/util/CMakeFiles/util.dir/StringUtil.cc.o.d"
+  "/Users/alkaid/Projects/workflow/src/util/URIParser.cc" "src/util/CMakeFiles/util.dir/URIParser.cc.o" "gcc" "src/util/CMakeFiles/util.dir/URIParser.cc.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
